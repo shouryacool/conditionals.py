051899c2975b4effa94540efae5fa5b8cd72e75d
shouryacool <84572733+shouryacool@users.noreply.github.com>
2021-11-13 19:27:35 +0530

    Add files via upload

---
+++ 01_Conditionals.py
@@ -0,0 +1,28 @@
+a=18
+# If-Else Ladder In Python
+if(a<3):
+    print("The Value Of a Is Less Then 3")
+
+elif(a<13):
+    print("The Value Of a Is greater Then 13")
+
+elif(a<17):
+    print("The Value Of a Is greater Then 17")
+
+else:
+     print("The value is Not Greater Them 3 or 7")   
+print("Done")
+
+# Multiple If Statemets 
+if(a<3):
+    print("The Value Of a Is Less Then 3")
+
+if(a<2):
+    print("The Value Of a Is Not greater Then 2")
+
+if(a>17):
+    print("The Value Of a Is greater Then 17")
+
+else:
+     print("The value is Not Greater  Then 0")   
+print("Done")
